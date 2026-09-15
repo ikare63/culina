04749a62230e7c6b9521569259e54378817deee0
ikare63 <lenaicandre59@gmail.com>
2026-09-15 23:16:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="culina-v5-4-5-cap-dinner";
+const CACHE="culina-v5-4-7-recipes-pass3-20260915";
 const CORE=["./","index.html","style.css","lenaic-bus.js","app.js","recettes.json","img/icon-192.png","img/favicon-32.png","favicon.ico","manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{})));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
